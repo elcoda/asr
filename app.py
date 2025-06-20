6aa028bc2f403e60bf11dd0929e9bd70768a7b38
Ivan Codarin <ivan.codarin@gmail.com>
2025-06-20 08:17:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def view():
     return render_template('view.html')
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5050)
